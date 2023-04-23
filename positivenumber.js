4a1c1822b40322a3a609da57de5fa5a98057bdcc
Divilisrikanth <srikanthdivili1994@gmail.com>
2023-04-23 12:04:32 +0530

    pushing the code to my github repo

---
+++ positivenumber.js
@@ -0,0 +1,14 @@
+let number=0
+let b=25
+let c=b;
+if(b>number){
+    for(let i=0;i<=2;i++){
+        c=c+2;
+    console.log(c)
+    
+}
+
+}else{
+    console.log("please enter a positive number")
+}
+
